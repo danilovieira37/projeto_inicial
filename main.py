b936adf96eefd187b8284570663d1224cd9302cf
unknown <danilovieira37@icloud.com>
2024-11-14 07:54:04 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print('jornada de dados')
+
+# variaveis
+idade = 34
+nome = "Luciano"
+
+# print
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# lista
+lista = [1,2,3,4,5]
+
+# metodos
+lista_invertida = lista.reverse()
